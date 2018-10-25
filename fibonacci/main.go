ebbe6c8bb6976bd1d69e5b17b347afbe29a51a21
Limian Wang <limian.wang@gmail.com>
2018-10-24 21:38:16 -0700

    fix the file formatting

--- fibonacci/main.go
+++ fibonacci/main.go
@@ -13,10 +13,35 @@ func fibonacci(n int, c chan int) {
 	close(c)
 }
 
+func fibonacci_alternate(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit!!")
+			return
+		}
+	}
+}
+
 func main() {
 	c := make(chan int, 10)
 	go fibonacci(cap(c), c)
 	for i := range c {
 		fmt.Println(i)
 	}
+
+	ch := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-ch)
+		}
+
+		quit <- 0
+	}()
+
+	fibonacci_alternate(c, quit)
 }
