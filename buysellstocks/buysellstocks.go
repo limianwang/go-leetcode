357f115fcd90788128741fb73ce1f3ac167c81db
Limian Wang <limian.wang@gmail.com>
2018-10-24 22:43:07 -0700

    leetcode#121. use differential to compute max

--- buysellstocks/buysellstocks.go
+++ buysellstocks/buysellstocks.go
@@ -1,14 +1,20 @@
 package buysellstocks
 
+import (
+	"math"
+)
+
 func maxProfit(prices []int) int {
 	length := len(prices)
 	max := 0
-	for i := length - 1; i >= 1; i-- {
-		for j := 0; j < i-1; j++ {
-			tmp := prices[i] - prices[j]
-			if max < tmp {
-				max = tmp
-			}
+	// Default set to max INT
+	minPrice := math.MaxInt64
+
+	for i := 0; i < length; i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		} else if prices[i]-minPrice > max {
+			max = prices[i] - minPrice
 		}
 	}
 
