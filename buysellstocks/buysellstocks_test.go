fb5cf36d753e4f621777c27f06c9939faaf7f300
Limian Wang <limian.wang@gmail.com>
2018-10-26 16:07:08 -0700

    leetcode#122. consecutive buy sell

--- buysellstocks/buysellstocks_test.go
+++ buysellstocks/buysellstocks_test.go
@@ -38,3 +38,30 @@ func TestMaxProfile(t *testing.T) {
 		})
 	}
 }
+
+func TestMaxSum(t *testing.T) {
+	tt := []struct {
+		name   string
+		input  []int
+		output int
+	}{
+		{
+			name:   "success1",
+			input:  []int{7, 1, 5, 3, 6, 4},
+			output: 7,
+		},
+		{
+			name:   "success2",
+			input:  []int{1, 2, 3, 4, 5},
+			output: 4,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			if profit := maxProfitSum(tc.input); profit != tc.output {
+				t.Fatalf("wanted %v but got %v", tc.output, profit)
+			}
+		})
+	}
+}
