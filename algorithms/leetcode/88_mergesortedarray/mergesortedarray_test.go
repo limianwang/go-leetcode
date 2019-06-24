b76f3cf5fc96d5a27f31b6e457e3c2b1de9f002f
Limian Wang <limian.wang@gmail.com>
2019-06-23 21:53:58 -0700

    Leetcode 88. Merge sorted array

---
+++ algorithms/leetcode/88_mergesortedarray/mergesortedarray_test.go
@@ -0,0 +1,50 @@
+package mergesortedarray
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMerge(t *testing.T) {
+	tt := []struct {
+		name     string
+		inputA   []int
+		sizeA    int
+		inputB   []int
+		sizeB    int
+		expected []int
+	}{
+		{
+			name:     "when input is valid",
+			inputA:   []int{1, 2, 3, 0, 0, 0},
+			sizeA:    3,
+			inputB:   []int{2, 5, 6},
+			sizeB:    3,
+			expected: []int{1, 2, 2, 3, 5, 6},
+		},
+		{
+			name:     "when input are both empty",
+			inputA:   []int{},
+			sizeA:    0,
+			inputB:   []int{},
+			sizeB:    0,
+			expected: []int{},
+		},
+		{
+			name:     "when input b is larger than a",
+			inputA:   []int{1, 2, 0, 0, 0},
+			sizeA:    2,
+			inputB:   []int{3, 4, 5},
+			sizeB:    3,
+			expected: []int{1, 2, 3, 4, 5},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			if merged := merge(tc.inputA, tc.sizeA, tc.inputB, tc.sizeB); !reflect.DeepEqual(merged, tc.expected) {
+				t.Fatalf("expected %v but got %v", tc.expected, merged)
+			}
+		})
+	}
+}
